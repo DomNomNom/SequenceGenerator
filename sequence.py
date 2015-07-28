d2531741b09b52af6926099df7d60d613fe17ac0
Dominik Schmid <DominikSchmid93@gmail.com>
2015-07-29 05:05:12 +1200

    Updated documentation

--- sequence.py
+++ sequence.py
@@ -7,11 +7,10 @@ from guessFunctions import guessFunctions
 Tries to generate a sequence of things given examples and an ellipsis.
 
 Example use:
-    sequence(1,...,10)
-    sequence(2,4,6,...) --> 2,4,6,8,10,12,14... (linear sequence)
-    sequence(2,4,8,...) --> 2,4,8,16,32,64,128... (exponential sequence)
-    sequence(2,4,8,...,64)
-    # maybe later: sequence('a',...'z')  # The lowercase alphabet from 'a'
+    sequence(1,...,10)      --> 1,2,3,4,5,6,7,8,9,10
+    sequence(2,4,6,...)     --> 2,4,6,8,10,12,14...   (linear sequence)
+    sequence(2,4,8,...)     --> 2,4,8,16,32,64,128... (exponential sequence)
+    sequence(2,4,8,...,64)  --> 2,4,8,16,32,64
 """
 def sequence(*args):
     assert args.count(Ellipsis) == 1, "you must give this function an ellipsis (...) otherwise it's no fun."
